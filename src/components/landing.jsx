b1680eccd91f71521daf5b7eb3acd42791695a40
Nidhish Gajjar <hacker@scigic.com>
2023-09-08 02:27:30 -0400

    Button adjustments

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -28,10 +28,10 @@ const Landing = () => {
     return (
         <div className="relative w-full h-screen overflow-hidden flex flex-col items-center justify-center bg-black">
             <video ref={videoRef} loop muted playsInline autoPlay className="absolute w-full h-full object-cover z-0">
-                <source src="https://chad-distributable.s3.amazonaws.com/aitakesorder.MOV" type="video/mp4" />
+                <source src="https://chad-distributable.s3.amazonaws.com/aitakesorder.MP4" type="video/mp4" />
             </video>
             <div className="absolute w-full h-full bg-black bg-opacity-50 z-10"></div>
-            <div className="absolute z-20 text-center text-white px-4 flex flex-col items-center">
+            <div className="absolute z-20 mb-10 text-center text-white px-4 flex flex-col items-center">
                 <div className="inline-block overflow-hidden border-r-2 border-white text-sm sm:text-md md:text-lg lg:text-xl xl:text-2xl">
                     <TypingText texts={['Let an AI take orders of your restaurant', 'Call us for a private demo']} />
                 </div>
